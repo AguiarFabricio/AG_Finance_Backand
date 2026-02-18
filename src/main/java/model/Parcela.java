6329b1484dc8d05b663cd56364265f6fc79051cd
Fabricio de Aguiar <aguiarfilho2006@gmail.com>
2026-02-17 21:09:46 -0300

    feat: adiciona entidade Parcela e finaliza estrutura do model

--- src/main/java/model/Parcela.java
+++ src/main/java/model/Parcela.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Fabricio de Aguiar
- */
-public class Parcela {
-    
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Parcela implements Serializable {
+
+    private int id;
+    private int numeroParcela;
+    private double valor;
+    private LocalDate dataVencimento;
+    private boolean paga;
+    private ContaParcelada contaParcelada;
+
+    public Parcela(int id, int numeroParcela, double valor, LocalDate dataVencimento, boolean paga, ContaParcelada contaParcela) {
+
+        this.id = id;
+        this.numeroParcela = numeroParcela;
+        this.valor = valor;
+        this.dataVencimento = dataVencimento;
+        this.paga = paga;
+        this.contaParcelada = contaParcelada;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNumeroParcela() {
+        return numeroParcela;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public boolean isPaga() {
+        return paga;
+    }
+
+    public ContaParcelada getContaParcelada() {
+        return contaParcelada;
+    }
+
 }
